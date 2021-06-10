9f302fcdeed1552092575e557bd0f2efa7ea7b8d
LeoSpadim <leospadim_new@outlook.com>
2021-06-09 23:09:39 -0300

    scene management
    - created transitions between the two scenes

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,10 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    //[?]Back to menu
+    public void Return()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
